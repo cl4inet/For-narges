b9d0cde412ddde1a5fd4d43a5513a5aef46558d8
cl4inet <parlaxxxjafariii@gmail.com>
2026-07-17 18:39:45 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+function checkPassword() {
+    const pass = document.getElementById("password").value;
 
+    if(pass === "7886"){
+        document.getElementById("lock").style.display="none";
+        document.getElementById("gift").style.display="block";
+    }else{
+        alert("Wrong password ❤️");
+    }
+}
